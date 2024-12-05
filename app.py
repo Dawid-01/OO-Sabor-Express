0112a3094a6b1d8ad613956b881747c0808ec9ae
Dawid-01 <dawid.hum1@gmail.com>
2024-12-05 18:40:09 -0300

    Aplicando herança e criando novas classes para o cardápio.

--- app.py
+++ app.py
@@ -1,17 +1,21 @@
 from Modelos.restaurante import Restaurante
+from Modelos.cardapio.bebida import Bebida
+from Modelos.cardapio.prato import Prato
+
 
 restaurante_praca = Restaurante('praça', 'Gourmet')
 restaurante_mexicano = Restaurante ('Los pollos hermanos', 'Mexicano')
 restaurante_italino = Restaurante ('Italianiii', 'italino')
 
-restaurante_mexicano.receber_avaliacao('Gui', 10)
-restaurante_mexicano.receber_avaliacao('Davi', 6)
+bebida_suco = Bebida('MelanJuyce' 5.00, 'grande')
+prato_massa = ('Lasanha', 15.00, 'Bolonhesa com mussarela e muito molho')
+
 
 
 restaurante_mexicano.alternar_estado()
 
 def main():
-    Restaurante.listar_restaurantes()
+   pass
 
 if __name__ == '__main__':
     main()
